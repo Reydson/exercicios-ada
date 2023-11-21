8f3005a423d9d69fad9e5db0347ac57554ef4ec2
Reydson Barros <reydson14@hotmail.com>
2023-11-21 00:14:21 -0300

    Finalização do projeto da fase 2

--- 02_Logica_de_Programacao/aula-7/script.js
+++ 02_Logica_de_Programacao/aula-7/script.js
@@ -1,15 +1,19 @@
 let tarefas = [
-    {id: 1, titulo: 'Testar aplicação', descricao: 'Realizar o teste da aplicação', categoria: 'Teste', deletado: false},
-    {id: 2, titulo: 'Atualizar aplicação', descricao: 'Realizar a atualização da aplicação', categoria: 'Atualização', deletado: false},
-    {id: 3, titulo: 'Testar rede', descricao: 'Realizar o teste da rede', categoria: 'Teste', deletado: false},
-    {id: 4, titulo: 'Atualizar computador', descricao: 'Realizar a atualização do computador', categoria: 'Atualização', deletado: false}
+    {id: 1, titulo: 'Testar aplicação', descricao: 'Realizar o teste da aplicação', categoria: 'Teste', deletado: false, vencimento: '2023-11-10T10:10:10'},
+    {id: 2, titulo: 'Atualizar aplicação', descricao: 'Realizar a atualização da aplicação', categoria: 'Atualização', deletado: true, vencimento: '2024-11-10T11:11:11'},
+    {id: 3, titulo: 'Testar rede', descricao: 'Realizar o teste da rede', categoria: 'Teste', deletado: false, vencimento: '2024-11-10T12:12:12'},
+    {id: 3, titulo: 'Teste geral', descricao: 'Realizar o teste geral', categoria: 'Teste', deletado: false, vencimento: ''},
+    {id: 4, titulo: 'Atualizar computador', descricao: 'Realizar a atualização do computador', categoria: 'Atualização', deletado: false, vencimento: '2024-11-10T13:13:13'}
 ];
 
 let filtros = {
     deletado: false,
     id: null,
-    categoria: null
+    categoria: null,
+    vencido: false
 }
+
+const filtroPadrao = {...filtros};
 
 const modalTarefa = new bootstrap.Modal(document.getElementById("modalTarefa"));
 const tituloModalTarefa = document.querySelector("#modalTarefa .modal-title");
@@ -33,6 +37,7 @@ function preparaModalEditar(id) {
     tarefaForm.elements['titulo'].value = tarefa.titulo;
     tarefaForm.elements['descricao'].value = tarefa.descricao;
     tarefaForm.elements['categoria'].value = tarefa.categoria;
+    tarefaForm.elements['vencimento'].value = tarefa.vencimento;
     modalTarefa.show();
 }
 
@@ -46,6 +51,11 @@ function atualizaBotoes() {
         botoes.classList.add('categoria');
     } else {
         botoes.classList.remove('categoria');
+    }
+    if(filtros.vencido) {
+        botoes.classList.add('vencido');
+    } else {
+        botoes.classList.remove('vencido');
     }
 }
 
@@ -65,6 +75,11 @@ function imprimeListaTarefas() {
     if(!filtros.deletado) {
         cloneTarefas = cloneTarefas.filter((tarefa) => !tarefa.deletado);
     }
+    if(filtros.vencido) {
+        cloneTarefas = cloneTarefas.filter((tarefa) => converteDataTimestamp(tarefa.vencimento) < converteDataTimestamp());
+    } else {
+        cloneTarefas = cloneTarefas.filter((tarefa) => tarefa.vencimento.length == 0 || converteDataTimestamp(tarefa.vencimento) > converteDataTimestamp());
+    }
     if(filtros.id) {
         cloneTarefas = cloneTarefas.filter((tarefa) => tarefa.id == filtros.id);
     }
@@ -78,6 +93,18 @@ function imprimeListaTarefas() {
             clone.querySelector('.id').innerHTML = '#'+tarefa.id;
             clone.querySelector('.titulo').innerHTML = tarefa.titulo;
             clone.querySelector('.descricao').innerHTML = tarefa.descricao;
+            if(tarefa.vencimento != '') {
+                const campoVencimento = clone.querySelector('.vencimento');
+                campoVencimento.innerHTML = converteDataFormatoBR(tarefa.vencimento);
+                campoVencimento.parentElement.classList.remove('d-none');
+                if(converteDataTimestamp(tarefa.vencimento) < converteDataTimestamp()) {
+                    clone.querySelector('.vencido').classList.remove('d-none')
+                } else {
+                    clone.querySelector('.naoVencido').classList.remove('d-none')
+                }
+            }
+            
+            
             const catClone = clone.querySelector('.categoria');
             if(tarefa.categoria) {
                 catClone.innerHTML = tarefa.categoria;
@@ -117,24 +144,26 @@ function tituloJaCadastrado(titulo, idIgnorada) {
     })
 }
 
-function adicionaTarefa(titulo, descricao, categoria) {
+function adicionaTarefa(titulo, descricao, categoria, vencimento) {
     tarefas.push({
         id: proximaId(),
         titulo,
         descricao,
         categoria,
-        deletado: false
+        deletado: false,
+        vencimento
     });
 }
 
-function atualizaTarefa(id, titulo, descricao, categoria) {
+function atualizaTarefa(id, titulo, descricao, categoria, vencimento) {
     tarefas = tarefas.map((tarefa) => {
         if(tarefa.id == id) {
             return {
                 ...tarefa,
                 titulo,
                 descricao,
-                categoria
+                categoria,
+                vencimento
             }
         }
         return tarefa;
@@ -165,12 +194,25 @@ function restauraTarefa(id) {
     });
 }
 
+function converteDataTimestamp(data) {
+    if(data) {
+        return new Date(data).getTime()
+    }
+    return new Date().getTime()
+}
+
+function converteDataFormatoBR(data) {
+    const objDate = new Date(data);
+    return objDate.toLocaleDateString("pt-BR") + ' ' + objDate.toLocaleTimeString("pt-BR");
+}
+
 function processaSalvarTarefa() {
     try {
         const id = tarefaForm.elements['id'].value;
         const titulo = tarefaForm.elements['titulo'].value;
         const descricao = tarefaForm.elements['descricao'].value;
         const categoria = tarefaForm.elements['categoria'].value;
+        const vencimento = tarefaForm.elements['vencimento'].value;
         if(titulo.length < 4) {
             throw new Error('O campo título deve ter pelo menos 4 caracteres');
         }
@@ -192,10 +234,14 @@ function processaSalvarTarefa() {
             throw new Error('O campo categoria deve ter pelo menos 5 caracteres');
         }
 
+        if(vencimento != '' && converteDataTimestamp(vencimento) < converteDataTimestamp()) {
+            throw new Error('A data de vencimento não pode ser inferior à data atual');
+        }
+
         if(id == -1) {
-            adicionaTarefa(titulo, descricao, categoria);
+            adicionaTarefa(titulo, descricao, categoria, vencimento);
         } else {
-            atualizaTarefa(id, titulo, descricao, categoria);
+            atualizaTarefa(id, titulo, descricao, categoria, vencimento);
         }
         modalTarefa.hide();
         imprimeListaTarefas();
@@ -206,6 +252,11 @@ function processaSalvarTarefa() {
 
 function toggleDeletados() {
     filtros.deletado = !filtros.deletado;
+    imprimeListaTarefas();
+}
+
+function toggleVencidos() {
+    filtros.vencido = !filtros.vencido;
     imprimeListaTarefas();
 }
 
@@ -237,7 +288,7 @@ function processarClique({target}) {
     }
 }
 
-function processaPesquisa({currentTarget}) {
+function processaPesquisa() {
     filtros.id = pesquisaInput.value;
     imprimeListaTarefas();
 }
@@ -253,13 +304,48 @@ function processaSairCategoria() {
     imprimeListaTarefas();
 }
 
+function processaMostarContadores() {
+    const qtdTarefasSemCategoria = tarefas.filter((tarefa) => tarefa.categoria.length == 0).length;
+    const contagemCategorias = [];
+    tarefas.forEach((tarefa) => {
+        if(tarefa.categoria) {
+            const posicao = contagemCategorias.findIndex((contagem) => contagem.nome == tarefa.categoria)
+            if(posicao != -1) {
+                contagemCategorias[posicao].quantidade++;
+            } else {
+                contagemCategorias.push({
+                    nome: tarefa.categoria,
+                    quantidade: 1
+                });
+            }
+        }
+    })
+    const contagemCategoriasString = contagemCategorias.map((contagem) => ' -> ' + contagem.nome + ': ' + contagem.quantidade).join('\n');
+    const qtdTarefasSemVencimento = tarefas.filter((tarefa) => tarefa.vencimento.length == 0).length;
+    const qtdTarefasVencidas = tarefas.filter((tarefa) => converteDataTimestamp(tarefa.vencimento) < converteDataTimestamp()).length;
+    const qtdTarefasNoPrazo = tarefas.filter((tarefa) => converteDataTimestamp(tarefa.vencimento) > converteDataTimestamp()).length;
+
+    alert(`Quantidade de tarefas na aplicação: ${tarefas.length}\nQuantidade de tarefas sem categoria: ${qtdTarefasSemCategoria}\nQuantidade de tarefas por categoria:\n${contagemCategoriasString}\nQuantidade de tarefas sem vencimento: ${qtdTarefasSemVencimento}\nQuantidade de tarefas vencidas: ${qtdTarefasVencidas}\nQuantidade de tarefas no prazo: ${qtdTarefasNoPrazo}`);
+}
+
+function processaResetarFiltros() {
+    filtros = {...filtroPadrao};
+    pesquisaInput.value = '';
+    imprimeListaTarefas();
+}
+
 document.querySelector('#adicionarTarefaBtn').addEventListener('click', preparaModalAdicionar);
 document.querySelector('#mostrarDeletadosBtn').addEventListener('click', toggleDeletados);
 document.querySelector('#ocultarDeletadosBtn').addEventListener('click', toggleDeletados);
+document.querySelector('#mostrarVencidosBtn').addEventListener('click', toggleVencidos);
+document.querySelector('#ocultarVencidosBtn').addEventListener('click', toggleVencidos);
 document.querySelector('#salvaTarefaBtn').addEventListener('click', processaSalvarTarefa);
 document.querySelector('#pesquisaBtn').addEventListener('click', processaPesquisa);
 document.querySelector('#limpaPesquisaBtn').addEventListener('click', processaLimpaPesquisa);
 document.querySelector('#sairCategoriaBtn').addEventListener('click', processaSairCategoria);
+document.querySelector('#mostarContadoresBtn').addEventListener('click', processaMostarContadores);
+document.querySelector('#resetarFiltrosBtn').addEventListener('click', processaResetarFiltros);
+
 listaTarefas.addEventListener('click', processarClique);
 
 imprimeListaTarefas();
