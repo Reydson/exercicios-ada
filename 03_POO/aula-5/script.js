f359c4dd52ce1731163c005be9e3c472b078faec
Reydson Barros <reydson14@hotmail.com>
2023-12-11 23:22:03 -0300

    Arquivos que faltaram

---
+++ 03_POO/aula-5/script.js
@@ -0,0 +1,36 @@
+//1. Sistema de Gerenciamento de Biblioteca
+class Livro {
+    constructor(nome, autor) {
+        this.nome = nome;
+        this.autor = autor;
+    }
+}
+
+class Biblioteca {
+    constructor() {
+        this.livros = [];
+    }
+    adicionarLivro(livro) {
+        this.livros.push(livro);
+    }
+    removerLivro(nomeLivro) {
+        this.livros = this.livros.filter((livro) => livro.nome != nomeLivro);
+    }
+    listarLivros() {
+        console.log(this.livros.map(({nome, autor}) => `${nome} (${autor})`).join('\n'));
+    }
+}
+
+const macunaima = new Livro('Macunaíma', 'Mário de Andrade');
+const domCasmurro = new Livro('Dom Casmurro', 'Machado de Assis');
+const brasCubas = new Livro('Memórias Póstumas de Brás Cubas', 'Machado de Assis');
+
+const biblioteca = new Biblioteca();
+biblioteca.adicionarLivro(macunaima);
+biblioteca.adicionarLivro(domCasmurro);
+biblioteca.adicionarLivro(brasCubas);
+
+biblioteca.listarLivros();
+biblioteca.removerLivro('Macunaíma');
+biblioteca.listarLivros();
+
